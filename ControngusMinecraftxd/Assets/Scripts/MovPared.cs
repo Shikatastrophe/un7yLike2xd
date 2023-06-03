d4384d9cb7738f43705ac8c62f0b2b7104a5a089
amauryxd <cdmx2472@amerike.edu.mx>
2023-06-03 14:56:16 -0600

    movimiento de escenario
    cuando destruyes la pared, el escenario ya se mueve

---
+++ ControngusMinecraftxd/Assets/Scripts/MovPared.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovPared : MonoBehaviour
+{
+    public GameObject nv;
+    Formulas formulas;
+    public float coolDown;
+
+    public float tiSigDis;
+
+    bool mover = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        formulas = new Formulas();
+        if (tiSigDis > 0 && mover)
+        {
+            tiSigDis -= Time.deltaTime;
+            gameObject.transform.position = formulas.Translacion(gameObject.transform.position, new Vector3(nv.transform.position.x * 0.1f, nv.transform.position.y, nv.transform.position.z));
+        }
+        if (tiSigDis <= 0)
+        {
+            //gameObject.transform.position = formulas.Translacion(gameObject.transform.position, new Vector3(nv.transform.position.x * 0.1f, nv.transform.position.y, nv.transform.position.z));
+            tiSigDis = coolDown;
+            mover = false;
+        }
+        
+    }
+
+    public void moverxd()
+    {
+        mover = true;
+    }
+}
--- ControngusMinecraftxd/Assets/Scripts/Vidapared.cs
+++ ControngusMinecraftxd/Assets/Scripts/Vidapared.cs
@@ -8,6 +8,8 @@ public class Vidapared : MonoBehaviour
     public int health = 100;
     public int damage = 10;
 
+    public GameObject paredd;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Bala"))
@@ -25,6 +27,7 @@ public class Vidapared : MonoBehaviour
         if (health <= 0)
         {
             sfxPlayer.Instance.Explodesfx();
+            paredd.GetComponent<MovPared>().moverxd();
             Destroy(gameObject);
             Debug.Log("Pared Destruida");
         }
