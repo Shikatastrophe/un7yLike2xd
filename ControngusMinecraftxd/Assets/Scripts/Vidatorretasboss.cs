5dc56f670105a94510c94ec0e770024b51919589
warcilac9 <warcilac9@gmail.com>
2023-06-05 09:07:41 -0600

    fdghdgsrfhg

---
+++ ControngusMinecraftxd/Assets/Scripts/Vidatorretasboss.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vidatorretasboss : MonoBehaviour
+
+{
+    public int health = 100;
+    public int damage = 10;
+
+    public GameObject torretas;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Bala"))
+        {
+            LoseHealth(damage);
+        }
+    }
+    public void LoseHealth(int ReduccionVida)
+    {
+        health -= ReduccionVida;
+        CheckHealth();
+    }
+    public void CheckHealth()
+    {
+        if (health <= 0)
+        {
+            sfxPlayer.Instance.Explodesfx();
+            Destroy(gameObject);
+            Debug.Log("Pared Destruida");
+        }
+    }
+}
